188471d2c70c87514c84dcdc19b26393b20165e1
aras370 <hesamehsani72@yahoo.com>
2025-06-28 10:59:00 +0330

    افزودن کلمه api به مسیر ها

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://cityexplorer-server-1.onrender.com/',
+  apiBaseUrl: 'https://cityexplorer-server-1.onrender.com/api/',
   imageBaseUrl: 'https://cityexplorer-server-1.onrender.com/Images/'
 };
